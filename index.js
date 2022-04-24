ef4bd2549809884e23f49717be7ca8ad3830881c
Luis <luis.mogueadp@gmail.com>
2022-04-24 11:24:12 -0500

    Se agrega proyecto

---
+++ index.js
@@ -0,0 +1,9 @@
+function parImpar(number1){
+    if ((number1 % 2) == 0) {
+        return "This is a even number"
+        }  else {
+        return "This is a odd number"
+        }
+    }
+    
+    console.log(parImpar(7))
